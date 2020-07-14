181538f56dadd1291c58000911a55af432c30646
Seugnwon Lee <65075764+lsw6684@users.noreply.github.com>
2020-07-14 18:22:27 +0900

    Add files via upload
    계산기

---
+++ 계산기.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char o;
+	int x, y;
+	while(1)
+	{
+		
+		printf("수식을 입력하세요 : ");
+		scanf("%d %c %d", &x, &o, &y);
+		if(o == '+')
+		{
+			printf("%d %c %d = %d\n", x, o, y, x + y);
+		}
+		else if(o == '*')
+		{
+			printf("%d %c %d = %d\n", x, o, y, x * y);
+		}
+		else if(o == '/')
+		{
+			printf("%d %c %d = %d\n", x, o, y, x / y);
+		}
+		else if(o == '-')
+		{
+			printf("%d %c %d = %d\n", x, o, y, x - y);
+		}
+		else if(o == '%')
+		{
+			printf("%d %c %d = %d\n", x, o, y, x % y);
+		}
+		else
+		{
+			printf("잘못된 입력입니다.\n");
+		}
+		getchar();
+		printf("프로그램을 종료하십니까? (Y/N)\n");
+		scanf("%c", &o);
+		if(o=='y'||o=='Y')
+		{
+			printf("프로그램을 종료합니다.");
+			break;
+		}
+		else if(o=='n'||o=='N')
+		{
+			continue;
+		}
+		else
+		{
+			printf("다시 입력하세요.");
+		}	
+	}
+	return 0;
+}
